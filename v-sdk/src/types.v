22895d9950fa475631ff8ba11e9e6b4edd93ac6b
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-03-22 08:13:14 -0600

    Create v-sdk/src/types.v

---
+++ v-sdk/src/types.v
@@ -0,0 +1,11 @@
+module kyc_sdk
+
+// Additional types.
+pub struct UserData {
+    pub:
+        user_id string
+        name string
+        email string
+        age int
+        country string
+}
